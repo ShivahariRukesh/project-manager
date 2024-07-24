5d4ba91f89bc7240f1855c9dce8765ceab6fbc9e
66Anony123 <478gnujnoej@gmail.com>
2024-07-24 13:15:32 +0545

    Router Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  return <>Welcome to the project management app</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { StyleSheetManager } from "styled-components";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
+          <App />
+        </StyleSheetManager>
+      </BrowserRouter>
+    </Suspense>
+  </React.StrictMode>
+);
